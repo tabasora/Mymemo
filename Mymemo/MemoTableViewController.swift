a08b0d83a45acd94a5db6477f71d24bfa617b13f
Macbook <macbook@macbooknomacbook-puro.local>
2020-11-25 21:35:05 +0900

    スワイプ削除機能

--- Mymemo/MemoTableViewController.swift
+++ Mymemo/MemoTableViewController.swift
@@ -64,25 +64,20 @@ class MemoTableViewController: UITableViewController {
         return "section-\(section)"
 }
    */
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+    //Override to support conditional editing of the table view.
+     //override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
+      //  return true
+    //}
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    //Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            self.memos.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
 
     /*
     // Override to support rearranging the table view.
